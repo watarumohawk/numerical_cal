b241282b3eedd94f463174c2bb62d0348a2c5dfb
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2015-05-26 19:56:04 +0900

    added No6

---
+++ No6/sample.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+#define f(x) x*exp(-3*x)
+
+
+int main() {
+	double x, x1, x2, dx, f0, f1, f2, f3;
+
+	printf("xを入力して下さい。x= ");
+	scanf("%lf", &x);
+
+	dx = 0.01;
+	x1 = x - dx;
+	x2 = x + dx;
+
+	f1 = ( f(x2) - f(x) )/dx;
+	f2 = ( f(x) - f(x1) )/dx;
+	f3 = ( f(x2) - f(x1))/(2*dx);
+	f0 = (1-3*x)*exp(-3*x);
+
+	printf("前進差分 f1 = %f\n", f1);
+	printf("後退差分 f2 = %f\n", f2);
+	printf("中心差分 f3 = %f\n", f3);
+	printf("正確な値 f0 = %f\n", f0);
+}
