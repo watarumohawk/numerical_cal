e60f50dfd9effa9c772ee1cd2a4523643dd3305e
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2015-05-30 17:14:48 +0900

    sin()とcos()の引数はラジアンなので、radの変換を追加

--- No2/problem.c
+++ No2/problem.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define g 9.8
+#define g 9.80665
 
 int main(void) {
 
-	double DetermineX(double v0, double deg, double t);
-	double DetermineY(double v0, double deg, double t);
-	double DetermineMag(double v0, double deg);
+	double DetermineX(double v0, double rad, double t);
+	double DetermineY(double v0, double rad, double t);
+	double DetermineMag(double v0, double rad);
 
-	double x, y, v0, deg, t, magnitude;
+	double x, y, v0, deg, rad, t, magnitude;
 
 	printf("初速度v0 (m/s)、角度θ（°）、時間tを入力して下さい。\n");
 	printf("v0 = ");
@@ -17,13 +17,14 @@ int main(void) {
 
 	printf("deg = ");
 	scanf("%lf", &deg);
+	rad = deg*(M_PI/180);
 
 	printf("t = ");
 	scanf("%lf", &t);
 
-	x = DetermineX(v0, deg, t);
-	y = DetermineY(v0, deg, t);
-	magnitude = DetermineMag(v0, deg);
+	x = DetermineX(v0, rad, t);
+	y = DetermineY(v0, rad, t);
+	magnitude = DetermineMag(v0, rad);
 
 	printf("x=%f y=%f\n", x, y);
 	printf("magnitude=%f \n", magnitude);
@@ -31,26 +32,26 @@ int main(void) {
 	return 0;
 }
 
-double DetermineX(double v0, double deg, double t) {
+double DetermineX(double v0, double rad, double t) {
 	double x;
 
-	x = v0*cos(deg)*t;
+	x = v0*cos(rad)*t;
 
 	return x;
 }
 
-double DetermineY(double v0, double deg, double t) {
+double DetermineY(double v0, double rad, double t) {
 	double y;
 
-	y = v0*sin(deg)*t - (1/2)*g*t*t;
+	y = v0*sin(rad)*t - (1/2)*g*t*t;
 
 	return y;
 }
 
-double DetermineMag(double v0, double deg) {
+double DetermineMag(double v0, double rad) {
 	double magnitude, v;
 
-	v = sqrt( v0*v0*( sin(deg)*sin(deg) + cos(deg)*cos(deg) ) );
+	v = sqrt( v0*v0*( sin(rad)*sin(rad) + cos(rad)*cos(rad) ) );
 	magnitude = abs(v);
 
 	return magnitude;
