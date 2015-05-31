1364dfb40825fc4dc07ebd97ba41c97671f858b0
Wataru Sekiguchi <wataru.sekiguchi416@gmail.com>
2015-05-31 18:04:35 +0900

    ミスを修正

--- No7/problem.c
+++ No7/problem.c
@@ -3,20 +3,20 @@
 #define f(x) sin(x)
 
 
-int main() {
+int main(void) {
 	double x, x1, x2, dx, f2;
 
 	printf("Δxを入力して下さい。Δx= ");
 	scanf("%lf", &dx);
-
-	x1 = x - dx;
-	x2 = x + dx;
 
 	FILE *data;
 	data = fopen("problem_data.csv", "w");
 
 	for (int i=0; i<360; i++) {
 		x = i*( M_PI/180. );
+		x1 = x - dx;
+		x2 = x + dx;
+
 		f2 = ( f(x2) - 2*f(x) + f(x1) )/(dx*dx);
 		printf("x = %f f2 = %f\n", x, f2);
 		fprintf(data, "%f, %f\n", x, f2);
@@ -25,4 +25,6 @@ int main() {
 	fclose(data);
 
 	printf("2階微分 f2 = %f\n", f2);
+
+	return 0;
 }
